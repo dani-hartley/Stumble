552581c16d8661bc52d99d081c772b27249fd3e3
ariellongoria <ariello97@hotmail.com>
2021-04-27 18:42:38 -0500

    added form label around input/select elements, linked script.js, tested submit functionality

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+// fetch("");
+
+var formEl = document.querySelector("#address-form");
+
+formEl.addEventListener("submit", function (event) {
+    event.preventDefault();
+    alert("let's-a go");
+});
