812662578618457517c6db36eb5154c17b085b3b
ariellongoria <ariello97@hotmail.com>
2021-04-27 20:54:07 -0500

    display brewery name on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
-// fetch("");
-
 var formEl = document.querySelector("#address-form");
+var inputEl = document.querySelector("#address");
+var selectEl = document.querySelector("#distance");
+var breweryContainer = document.querySelector("#breweries");
 
-formEl.addEventListener("submit", function (event) {
+var fetchApiData = function () {
+    var apiUrl = "https://api.openbrewerydb.org/breweries?per_page=5";
+
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => createCard(data));
+};
+
+var formSubmit = function (event) {
     event.preventDefault();
-    alert("let's-a go");
-});
+    fetchApiData();
+};
+
+var createCard = function (breweries) {
+    for (var i = 0; i < breweries.length; i++) {
+        var breweryCard = document.createElement("div");
+        breweryCard.className = "brewery-card";
+        breweryCard.textContent = breweries[i].name;
+        breweryContainer.appendChild(breweryCard);
+    }
+};
+
+formEl.addEventListener("submit", formSubmit);
