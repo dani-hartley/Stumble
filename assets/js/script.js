197f5882947dd33ab4951717470ccedbb0a9612d
ariellongoria <ariello97@hotmail.com>
2021-05-05 00:54:03 -0500

    cleaner header and new favorites button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,9 @@ var formEl = document.querySelector("#address-form");
 var inputEl = document.querySelector("#search");
 var selectEl = document.querySelector("#distance");
 var breweryContainer = document.querySelector("#brewery-class");
-<<<<<<< HEAD
 var breweryRow = document.querySelector("#brewery-row");
 var breweryClass = document.querySelector("#brewery-class");
-var favSide = document.querySelector("#slide-out");
-=======
-var breweryRow = document.querySelector('#brewery-row');
-var breweryClass = document.querySelector('#brewery-class');
 var favSide = document.getElementById("slide-out");
->>>>>>> 12264fe8de7bf7e76e0f688d73dc66f035908e87
 
 var fetchZipData = function (zip, distance) {
     var fetchUrl =
@@ -71,23 +65,22 @@ var createCard = function (breweries) {
         //Some breweries have no address data, so we check for it before appending anything
         if (breweries[i].street) {
             breweryContainer.appendChild(breweryCard);
-            
         }
     }
     //Save to local storage
-    $('.addToFavBtn').on('click', function () {
-        console.log((this).children[0].innerHTML);
-        (this).children[0].innerHTML = "star";
+    $(".addToFavBtn").on("click", function () {
+        console.log(this.children[0].innerHTML);
+        this.children[0].innerHTML = "star";
         var savName = $(this).siblings(".brewery-name").text();
         var savAddress = $(this).siblings(".brewery-address").text();
         var savType = $(this).siblings(".brewery-type").text();
-        console.log(savName, savAddress, savType)
+        console.log(savName, savAddress, savType);
         var favBrewery = {
             name: savName,
             address: savAddress,
-            type: savType
+            type: savType,
         };
-        var savFavs = localStorage.getItem('savFavs');
+        var savFavs = localStorage.getItem("savFavs");
         if (!savFavs) {
             savFavs = [];
         } else {
@@ -98,26 +91,22 @@ var createCard = function (breweries) {
         var newFav = JSON.stringify(savFavs);
         localStorage.setItem("favorite", newFav);
         favDisplay();
-    })
-
+    });
 };
 
 function favDisplay() {
-    var savFav = localStorage.getItem('savFavs');
+    var savFav = localStorage.getItem("savFavs");
     savFav = JSON.parse(savFav);
-    
+
     //Display Local Storage
     if (savFav !== null) {
-        for (var i=0; i < savFav.length; i++) {
+        for (var i = 0; i < savFav.length; i++) {
             var createLi = document.createElement("li");
-            createLi.innerText = savFav[i].name + ": " + 
-            savFav[i].address + "(" + 
-            savFav[i].type + ")";
+            createLi.innerText = savFav[i].name + ": " + savFav[i].address + "(" + savFav[i].type + ")";
             favSide.appendChild(createLi);
         }
     }
-};
-
+}
 
 formEl.addEventListener("submit", formSubmit);
 
@@ -153,5 +142,3 @@ document.addEventListener("DOMContentLoaded", function () {
     var elems = document.querySelectorAll(".sidenav");
     var instances = M.Sidenav.init(elems);
 });
-
-
